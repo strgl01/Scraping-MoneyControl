3794a94011a7efeec6a95f971b5ad211f9d709fc
strgl01 <ranjanavinash091@gmail.com>
2026-02-11 16:42:59 +0530

    added code for driver initialisation and its testing in files driver.py and test_driver.py respectively

---
+++ driver.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+
+"""
+This piece of code initialise a driver in future if required any change in functionality of driver this is the module to work with. Code in this module is written in general way and can be used with other scrappers
+"""
+
+class Driver():
+
+    """I have kept below variable class level because the value don't depend on the instaces of class"""
+
+    path: str = r"C:\Users\dell\Desktop\geckodriver.exe"
+    service = Service(path)
+    options = Options()
+    options.set_preference('dom.webnotification.enabled', False)
+
+    def __init__(self):
+        self.create()
+
+    def create(self):
+        try:
+            self.driver = webdriver.Firefox(service=Driver.service, options=Driver.options)
+            return self.driver
+        except Exception as ex:
+            print(f'{ex!r}')
+            self.driver.close()
---
+++ test_driver.py
@@ -0,0 +1,8 @@
+from driver import Driver
+
+def test_driverinitialised():
+    driver = Driver()
+    assert driver
+
+
+
